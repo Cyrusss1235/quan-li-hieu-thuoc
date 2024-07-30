68bdb2c933af98175d632223b6a5013453cb661b
Cyrusss1235 <nguyenduccuong1235@gmail.com>
2024-07-30 18:09:56 +0700

    code tao bang va truy van

---
+++ THEMDL.sql
@@ -0,0 +1,102 @@
+﻿INSERT INTO NHOMTHUOC (MANHOMTHUOC, TENNHOMTHUOC) VALUES
+('1', N'Thuốc kháng sinh'),
+('2', N'Thuốc giảm đau và kháng viêm'),
+('3', N'Thuốc điều trị tim mạch'),
+('4', N'Thuốc điều trị tiểu đường'),
+('5', N'Thuốc chống dị ứng'),
+('6', N'Thuốc chống viêm'),
+('7', N'Thuốc điều trị loét dạ dày - tá tràng'),
+('8', N'Thuốc điều trị hen suyễn và COPD'),
+('9', N'Thuốc điều trị ung thư'),
+('10', N'Thuốc chống trầm cảm'),
+('11', N'Thuốc chống loạn thần'),
+('12', N'Thuốc kháng virus'),
+('13', N'Thuốc chống đông máu'),
+('14', N'Thuốc an thần và gây ngủ'),
+('15', N'Thuốc kháng nấm');
+INSERT INTO KHACHHANG (MAKHACHHANG, TENKHACHHANG, DIACHI, EMAIL, DIENTHOAI) VALUES
+('KH001', 'Nguyen Van Cuong', '123 Le Loi, Ha Noi', 'nguyenvana@example.com', '0901234567'),
+('KH002', 'Tran Thi Thanh', '456 Tran Hung Dao, Ha Noi', 'tranthib@example.com', '0912345678'),
+('KH003', 'Le Thi Chi', '789 Nguyen Hue, Ha Noi', 'lethic@example.com', '0923456789'),
+('KH004', 'Pham Thi Hai', '123 Nguyen Trai, Ha Noi', 'phamthig@example.com', '0908765432'),
+('KH005', 'Bui Van Huy', '456 Ly Thuong Kiet, Ha Noi', 'buivanh@example.com', '0917654321');
+INSERT INTO NHANVIEN (MANHANVIEN, HO, TEN, NGAYSINH, NGAYLAMVIEC, DIACHI, DIENTHOAI, LUONGCOBAN) VALUES
+('NV001', 'Nguyen', 'Van Dong', '1980-05-15', '2010-01-01', '321 Hai Ba Trung, Ha Noi', '0909876543', 10000000),
+('NV002', 'Tran', 'Thi Hong', '1985-07-20', '2012-02-01', '654 Pham Van Dong, Ha Noi', '0918765432', 12000000),
+('NV003', 'Le', 'Van Tai', '1990-09-25', '2015-03-01', '987 Le Thanh Ton, Ha Noi', '0927654321', 11000000),
+('NV004', 'Pham', 'Thi Khanh', '1982-04-18', '2011-04-01', '321 Tran Phu, Ha Noi', '0908765432', 10500000),
+('NV005', 'Bui', 'Van Khai', '1988-08-22', '2013-05-01', '654 Dien Bien Phu, Ha Noi', '0917654321', 11500000);
+INSERT INTO NHACUNGCAP (MACONGTY, TENCONGTY, DIACHI, DIENTHOAI, EMAIL) VALUES
+('NCC001', N'Công ty cổ phần dược liệu Trung ương 2', '123 Le Loi, TP HCM', '0901234567', 'contact@duoca.com'),
+('NCC002', N'Tổng công ty dược Việt Nam – CTCP', '456 Tran Hung Dao, Ha Noi', '0912345678', 'contact@duocb.com'),
+('NCC003', N'Công ty cổ phần dược phẩm Trung ương CPC1', '789 Nguyen Hue, Da Nang', '0923456789', 'contact@duocc.com'),
+('NCC004', N'Công ty TNHH dược phẩm Khương Duy', N'NN1-NN1A Bạch Mã, Phường 15, Quận 10, TP.HCM', '0912345678', 'kdco@khuongduy.com.vn'),
+('NCC005', N'Công ty cổ phần dược phẩm thiết bị y tế Hà Nội (Hapharco)', N'Số 2 Hàng Bài, quận Hoàn Kiếm, TP. Hà Nội', '0923456789', 'Contact@hapharco.com.vn');
+
+INSERT INTO HOADONBAN (SOHOADON, MAKHACHHANG, MANHANVIEN, NGAYMUA) VALUES
+('HD001', 'KH001', 'NV001', '2024-01-15'),
+('HD002', 'KH002', 'NV002', '2024-02-20'),
+('HD003', 'KH003', 'NV003', '2024-03-25'),
+('HD004', 'KH004', 'NV004', '2024-04-15'),
+('HD005', 'KH005', 'NV005', '2024-05-20');
+INSERT INTO HOADONBAN (SOHOADON, MAKHACHHANG, MANHANVIEN, NGAYMUA)
+VALUES
+('HD006', 'KH004', 'NV004', '2024-05-01'),
+('HD007', 'KH005', 'NV005', '2024-05-05');
+
+INSERT INTO CHITIETHOADON (SOHOADON, MATHUOC, GIABAN, SOLUONG, MUCGIAMGIA) VALUES
+('HD001', 'T001', 50000, 2, 10),
+('HD001', 'T002', 30000, 1, 5),
+('HD002', 'T003', 45000, 3, 15),
+('HD002', 'T004', 35000, 4, 0),
+('HD003', 'T005', 20000, 5, 20),
+('HD003', 'T006', 40000, 2, 10),
+('HD004', 'T016', 45000, 3, 10),
+('HD004', 'T017', 65000, 2, 5),
+('HD005', 'T018', 75000, 4, 15),
+('HD005', 'T019', 55000, 1, 0),
+('HD006', 'T015', 65000, 2, 5),
+('HD006', 'T012', 75000, 4, 15),
+('HD006', 'T013', 65000, 2, 5),
+('HD007', 'T011', 75000, 4, 15),
+('HD007', 'T010', 65000, 2, 5),
+('HD007', 'T008', 75000, 4, 15),
+('HD006', 'T007', 65000, 2, 5),
+('HD005', 'T003', 75000, 4, 15);
+INSERT INTO THUOC (MATHUOC, TENTHUOC, MACONGTY, MANHOMTHUOC, NSX, HSD, SOLUONG, DONVITINH, GIATHUOC) VALUES
+('T001', 'Amoxicillin', 'NCC002', '1', '2023-01-01', '2025-01-01', 100, N'Hộp', 50000),
+('T002', 'Ibuprofen', 'NCC001', '2', '2023-02-01', '2025-02-01', 200, N'Hộp', 30000),
+('T003', 'Lisinopril', 'NCC002', '3', '2023-03-01', '2025-03-01', 150, N'Hộp', 45000),
+('T004', 'Metformin', 'NCC002', '4', '2023-04-01', '2025-04-01', 250, N'Hộp', 35000),
+('T005', 'Loratadine', 'NCC001', '5', '2023-05-01', '2025-05-01', 300, N'Hộp', 20000),
+('T006', 'Prednisone', 'NCC001', '6', '2023-06-01', '2025-06-01', 120, N'Hộp', 40000),
+('T007', 'Omeprazole', 'NCC001', '7', '2023-07-01', '2025-07-01', 130, N'Hộp', 50000),
+('T008', 'Salbutamol', 'NCC00', '8', '2023-08-01', '2025-08-01', 140, N'Hộp', 45000),
+('T009', 'Cyclophosphamide', 'NCC001', '9', '2023-09-01', '2025-09-01', 110, N'Hộp', 60000),
+('T010', 'Fluoxetine', 'NCC004', '10', '2023-10-01', '2025-10-01', 115, N'Hộp', 55000),
+('T011', 'Risperidone', 'NCC004', '11', '2023-11-01', '2025-11-01', 125, N'Hộp', 50000),
+('T012', 'Oseltamivir', 'NCC004', '12', '2023-12-01', '2025-12-01', 135, N'Hộp', 70000),
+('T013', 'Warfarin', 'NCC001', '13', '2024-01-01', '2026-01-01', 145, N'Hộp', 40000),
+('T014', 'Diazepam', 'NCC004', '14', '2024-02-01', '2026-02-01', 155, N'Hộp', 35000),
+('T015', 'Fluconazole', 'NCC001', '15', '2024-03-01', '2026-03-01', 165, N'Hộp', 60000),
+('T016', 'Penicillin', 'NCC002', '1', '2023-01-15', '2025-01-15', 180, N'Hộp', 45000),
+('T017', 'Ceftriaxone', 'NCC002', '1', '2023-02-20', '2025-02-20', 170, N'Hộp', 65000),
+('T018', 'Azithromycin', 'NCC003', '1', '2023-03-25', '2025-03-25', 190, N'Hộp', 75000),
+('T019', 'Ciprofloxacin', 'NCC003', '1', '2023-04-30', '2025-04-30', 160, N'Hộp', 55000),
+('T020', 'Diclofenac', 'NCC001', '2', '2023-05-10', '2025-05-10', 140, N'Hộp', 35000),
+('T021', 'Morphine', 'NCC005', '2', '2023-06-15', '2025-06-15', 130, N'Hộp', 85000),
+('T022', 'Amlodipine', 'NCC004', '3', '2023-07-20', '2025-07-20', 120, N'Hộp', 40000),
+('T023', 'Atorvastatin', 'NCC005', '3', '2023-08-25', '2025-08-25', 110, N'Hộp', 30000),
+('T024', 'Insulin glargine', 'NCC004', '4', '2023-09-30', '2025-09-30', 100, N'Hộp', 95000),
+('T025', 'Diphenhydramine', 'NCC005', '5', '2023-10-05', '2025-10-05', 90, N'Hộp', 25000);
+
+
+
+select*from nhomthuoc
+select*from thuoc
+select*from NHACUNGCAP
+select*from HOADONBAN
+select*from CHITIETHOADON
+select*from NHANVIEN
+select*from KHACHHANG
+
---
+++ truyvan.sql
@@ -0,0 +1,186 @@
+﻿
+-- 8 CÂU TRUY VẤN
+--1. Đưa ra thông tin chi tiết của hóa đơn HD006
+SELECT MATHUOC,SOLUONG,SUM((GIABAN*SOLUONG)-MUCGIAMGIA*(GIABAN*SOLUONG)/100) 'TỔNG TIỀN'
+FROM CHITIETHOADON 
+WHERE SOHOADON = 'HD006'
+GROUP BY MATHUOC,SOLUONG;
+--2. Đưa ra danh sách thuốc và nhà cung cấp của thuốc
+SELECT THUOC.TENTHUOC, NHACUNGCAP.TENCONGTY
+FROM THUOC INNER JOIN NHACUNGCAP ON THUOC.MACONGTY = NHACUNGCAP.MACONGTY;
+--3. Đưa ra danh sách các hóa đơn bán trong tháng 5 năm 2024
+SELECT * FROM HOADONBAN
+WHERE NGAYMUA LIKE '2024-05%';
+--4 . Tính tổng số tiền của THUOC đã bán
+SELECT CT.MATHUOC,TENTHUOC,SUM((GIABAN*CT.SOLUONG)-MUCGIAMGIA*(GIABAN*CT.SOLUONG)/100) 'TỔNG TIỀN'
+FROM CHITIETHOADON CT INNER JOIN THUOC MT ON CT.MATHUOC=MT.MATHUOC
+GROUP BY CT.MATHUOC, TENTHUOC;
+--5. Đưa ra những khách hàng mua hàng với tổng tiền cao nhất.
+SELECT TOP 1 WITH TIES KH.TENKHACHHANG,SUM((GIABAN*CT.SOLUONG)-MUCGIAMGIA*(GIABAN*CT.SOLUONG)/100) 'TỔNG TIỀN'
+FROM KHACHHANG KH INNER JOIN HOADONBAN HD ON KH.MAKHACHHANG=HD.MAKHACHHANG INNER JOIN CHITIETHOADON CT ON HD.SOHOADON=CT.SOHOADON
+GROUP BY KH.TENKHACHHANG
+ORDER BY [TỔNG TIỀN] DESC;
+--6 Đưa ra danh sách khách hàng có tổng số tiền mua hàng lớn hơn KH001
+SELECT KH.MAKHACHHANG,TENKHACHHANG,SUM((CT.SOLUONG*GIABAN)-(CT.SOLUONG*GIABAN*MUCGIAMGIA/100)) 'TONG TIEN'
+FROM KHACHHANG KH INNER JOIN HOADONBAN HD ON KH.MAKHACHHANG=HD.MAKHACHHANG INNER JOIN CHITIETHOADON CT ON HD.SOHOADON=CT.SOHOADON
+GROUP BY KH.MAKHACHHANG,TENKHACHHANG
+HAVING SUM((CT.SOLUONG*GIABAN)-(CT.SOLUONG*GIABAN*MUCGIAMGIA/100))>(SELECT SUM((CT.SOLUONG*GIABAN)-(CT.SOLUONG*GIABAN*MUCGIAMGIA/100)) 'TONG TIEN'
+FROM HOADONBAN HD INNER JOIN CHITIETHOADON CT ON HD.SOHOADON=CT.SOHOADON
+WHERE HD.MAKHACHHANG='KH001')
+--7 Đưa ra danh sách thuốc chưa từng được bán
+SELECT *
+FROM THUOC 
+WHERE MATHUOC NOT IN (SELECT MATHUOC FROM CHITIETHOADON GROUP BY MATHUOC)
+--8 Đưa ra nhà cung cấp cung cấp lượng mã thuốc lớn hơn 5
+SELECT NCC.MACONGTY,NCC.TENCONGTY,COUNT(THUOC.MATHUOC) 'SO MA THUOC'
+FROM NHACUNGCAP NCC INNER JOIN THUOC ON NCC.MACONGTY=THUOC.MACONGTY
+GROUP BY NCC.MACONGTY,NCC.TENCONGTY
+HAVING COUNT(THUOC.MATHUOC)>5
+--9 Đưa ra thuốc có hạn sử dụng lớn hơn 3 tháng tính từ ngày hiện tại
+
+
+-------------------------------------------------------------------------------
+
+--5 CÂU THỦ TỤC
+--1 Thủ tục tính tổng số lượng của 2 thuốc bất kì
+create procedure sl2mathang(@mh1 nvarchar(20),@mh2 nvarchar(20))
+as
+	declare @sl1 int
+	declare @sl2 int
+	declare @tongsl int
+	if  not exists (select mathuoc from THUOC where MATHUOC=@mh1)
+	print N'Mã thuốc '+@mh1+ N' không tồn tại'
+	else if  not exists (select mathuoc from THUOC where MATHUOC=@mh2)
+	print N'Mã thuốc '+@mh2+ N' không tồn tại'
+	else begin
+	select @sl1=soluong from THUOC where mathuoc=@mh1
+	select @sl2=soluong from THUOC where mathuoc=@mh2 
+	select @tongsl=@sl1+@sl2
+	print 'Tong so luong 2 mat hang='+ str(@tongsl)
+	end
+	select* from thuoc
+	sl2mathang T001,T00200
+--2 thủ tục đưa ra tổng số lượng 2 nhóm thuốc bất kì
+create procedure sl2nhomthuoc(@mh1 nvarchar(20),@mh2 nvarchar(20))
+as
+	declare @sl1 int
+	declare @sl2 int
+	declare @tongsl int
+	if  not exists (select MANHOMTHUOC from THUOC where MANHOMTHUOC=@mh1)
+	print N'Nhóm thuốc '+@mh1+N' không tồn tại'
+	else if  not exists (select MANHOMTHUOC from THUOC where MANHOMTHUOC=@mh2)
+	print N'Nhóm thuốc '+@mh2+N' không tồn tại'
+	else begin
+	select @sl1=sum(soluong) from THUOC,NHOMTHUOC where thuoc.MANHOMTHUOC=@mh1 and thuoc.MANHOMTHUOC=NHOMTHUOC.MANHOMTHUOC 
+	select @sl2=sum(soluong) from THUOC,NHOMTHUOC where thuoc.MANHOMTHUOC=@mh2 and thuoc.MANHOMTHUOC=NHOMTHUOC.MANHOMTHUOC 
+	select @tongsl=@sl1+@sl2
+	print 'Tong so luong 2 mat hang='+ str(@tongsl)
+	end
+--3 thủ tục đưa ra \những mã thuốc có hạn sử dụng nhỏ hơn 3 tháng kể từ ngày bất kì
+CREATE PROCEDURE LayThuocHanSuDungDuoi3Thang(@ngay Date)
+as
+BEGIN
+    
+    DECLARE @NgaySau3Thang DATE;
+    select @NgaySau3Thang = DATEADD(MONTH, +3, @Ngay);
+
+    SELECT *
+    FROM 
+        THUOC
+    WHERE 
+        HSD <= @NgaySau3Thang
+		and
+		HSD > @Ngay
+END;
+LayThuocHanSuDungDuoi3Thang '2025-05-20'
+--4 thủ tục đưa ra thông tin cảu một khách hàng bất kì
+create proc thongtinkh (@kh nvarchar(20))
+as
+begin
+if  not exists (select makhachhang from KHACHHANG where MAKHACHHANG=@kh)
+	print N'Khách hàng không tồn tại'
+else 
+Select * From KHACHHANG where MAKHACHHANG=@kh
+end
+--5 thủ tục so sánh số lượng cảu 2 mã thuốc bất kì
+create procedure sosanhsoluong(@mh1 nvarchar(20),@mh2 nvarchar(20))
+as
+	declare @sl1 int
+	declare @sl2 int
+	declare @tongsl int
+	
+	if  not exists (select mathuoc from THUOC where MATHUOC=@mh1)
+	print N'Mã thuốc '+@mh1+N' không tồn tại'
+	else if  not exists (select mathuoc from THUOC where MATHUOC=@mh2)
+	print N'Mã thuốc '+@mh2+N' không tồn tại'
+	else begin
+	select @sl1=soluong from THUOC where mathuoc=@mh1
+	select @sl2=soluong from THUOC where mathuoc=@mh2 
+	if(@sl1>@sl2)
+	print N'Mã thuốc'+@mh1+N'có số lượng nhiều hơn!'
+	else
+	print N'Mã thuốc'+@mh2+N'có số lượng nhiều hơn!'
+	end
+-------------------------------------------------------------------------------
+
+-- 5 CÂU TRIGGER
+--1 hạn sử dụng phải lớn hơn ngày sản xuất khi thêm dữ liệu
+CREATE TRIGGER trg_CHECK_HSD
+ON THUOC
+For INSERT
+AS
+BEGIN
+    IF EXISTS (SELECT 1 FROM INSERTED WHERE HSD <= NSX)
+    BEGIN
+        print N'Hạn sử dụng (HSD) phải lớn hơn ngày sản xuất (NSX).';
+        ROLLBACK TRANSACTION;
+    END
+    ELSE
+    BEGIN
+        INSERT INTO THUOC (MATHUOC, TENTHUOC, MACONGTY, MANHOMTHUOC, NSX, HSD, SOLUONG, DONVITINH, GIATHUOC)
+        SELECT MATHUOC, TENTHUOC, MACONGTY, MANHOMTHUOC, NSX, HSD, SOLUONG, DONVITINH, GIATHUOC
+        FROM INSERTED;
+    END
+END;
+--2 giảm số lượng thuốc khi thêm dữ liệu vào bảng chi tiết đơn hàng
+create trigger trg_insert
+on chitiethoadon
+for insert
+as
+update thuoc
+set thuoc.SOLUONG= thuoc.SOLUONG-inserted.soluong
+from THUOC inner join inserted on thuoc.mathuoc=inserted.mathuoc
+--3 tăng số lượng khi xoá dữ liệu bảng chi tiết đơn hàng
+create trigger trg_delete
+on chitiethoadon
+for delete
+as
+update thuoc
+set SOLUONG=thuoc.soluong+deleted.soluong
+from thuoc inner join deleted on thuoc.mathuoc=deleted.mathuoc
+--4 sửa dữ liệu bảng thuốc khi sửa dữ liệu bảng chi tiết đơn hàng
+create trigger trg_update
+on chitiethoadon
+for update
+as
+update thuoc
+set  soluong=thuoc.soluong-(inserted.soluong-deleted.soluong)
+from thuoc inner join deleted on thuoc.mathuoc=deleted.mathuoc 
+inner join inserted on thuoc.mathuoc=inserted.mathuoc
+--5 bắt lỗi thêm dữ liệu vào chi tiết đặt hàng số lượng phải lớn hơn 0
+create trigger trg_sl
+on chitiethoadon
+for insert
+as
+begin 
+if exists (select soluong from inserted where soluong<1)
+begin
+	print N'Vui Lòng Nhập Số Lượng Lớn Hơn 0'
+	rollback transaction
+end
+else
+BEGIN
+INSERT INTO CHITIETHOADON (SOHOADON, MATHUOC, GIABAN, SOLUONG, MUCGIAMGIA)
+SELECT SOHOADON, MATHUOC, GIABAN, SOLUONG, MUCGIAMGIA FROM INSERTED;
+    END
+	end;
